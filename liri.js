2e1cb7b97dba672ea3cef249273a4265b5818014
Paulina Cortez <paulina.a.cortez@gmail.com>
2019-05-17 09:53:47 -0700

    spotify working

--- liri.js
+++ liri.js
@@ -4,28 +4,15 @@ require("dotenv").config();
 
 var inquirer = require("inquirer");
 var axios = require("axios");
-// var keys = require("./keys.js");
-
-// var spotify = new Spotify(keys.spotify);
-
-
-
-
-
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 
 
 var userCommand = process.argv[2];
 var searchItem = process.argv[3];
 
-// var newDate;
-// function displayTime () {
-  
-//   console.log("date: " + date)
-//   newDate = date.toLocaleDateString();
-// }
-
 var concertThis = function() {
-
   axios.get("https://rest.bandsintown.com/artists/" + searchItem + "/events?app_id=codingbootcamp")
   .then(function(response) {
       var d = new Date(response.data[0].datetime);
@@ -40,6 +27,26 @@ var concertThis = function() {
   })
 }
   
+var spotifyThisSong = function () {
+  if (searchItem === undefined) {
+    searchItem = "The Sign by Ace of Base";
+  }
+  spotify
+  .search({ type: 'track', query: searchItem, limit: 2})
+  .then(function(response) {
+    // console.log(response.tracks.items[0].album.artists[0].external_urls);
+    console.log("+++++++++++++++++" +
+    "\nArtist: " + response.tracks.items[0].album.artists[0].name +
+    "\nSong: " + searchItem + 
+    "\nPreview link: " + response.tracks.items[0].album.preview_url +
+    "\nOpen link: " + response.tracks.items[0].album.external_urls.spotify +
+    "\nAlbum: " + response.tracks.items[0].album.name +
+    "\n+++++++++++++++++")
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+}
 
 
 var movieThis = function() {
@@ -59,25 +66,20 @@ var movieThis = function() {
       "\nActors: " + response.data.Actors +
       "\n=========================")
   })
-  // if (searchItem === undefined) {
-  //   searchItem = "Mr. Nobody"
-  // }
   }
-
-
 
 
 //    * `concert-this`
 
 if (userCommand === "concert-this") {
-  // console.log("concertThis()")
   concertThis();
-}
+} 
 
 //    * `spotify-this-song`
 
 else if (userCommand === "spotify-this-song") {
-  console.log("spotifyThisSong()")
+  // console.log("spotifyThisSong()")
+  spotifyThisSong();
 }
 
 //    * `movie-this`
